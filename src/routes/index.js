3d34d79382f24bca64b36b5ec9e65052859a9064
Vũ Trần Minh Hoàng <hoang.vutranminh@gmail.com>
2024-05-14 09:24:41 +0700

    update lại route products

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,9 @@ const express = require('express')
 const router = express.Router()
 
 router.use('/v1/api', require('./access/publicIndex'))
-router.use('/v1/api/product', require('./product/publicIndex'))
+router.use('/v1/api/products', require('./product/publicIndex'))
 router.use('/v1/api', require('./access'))
-router.use('/v1/api/product', require('./product'))
+router.use('/v1/api/products', require('./product'))
 router.use('/v1/api', require('./apiKey'))
 
 module.exports = router
